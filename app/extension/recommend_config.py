580fcf4611262435370161906adda7067f57c728
caotingv <caotingv@qq.com>
2023-06-29 17:10:07 +0800

    fix extend bug

--- app/extension/recommend_config.py
+++ app/extension/recommend_config.py
@@ -1,7 +1,7 @@
 import json
 
 from common import types
-from deploy.recommend_config import ReckRecommendConfigCommon
+from deploy.recommend_config import ReckRecommendConfigCommon, ShowRecommendConfig
 from models.deploy_history import DeployHistoryModel
 
 
@@ -12,47 +12,51 @@ class ExtendReckRecommendConfigCommon(ReckRecommendConfigCommon):
 
     def post(self):
         nodes_info = self.get_nodes_from_request()
+        ceph_service_flag = nodes_info['cephServiceFlag']
+        local_service_flag = nodes_info['localServiceFlag']
         storage_list = nodes_info["storages"]
         self.classify_disks(storage_list)
 
-        if self.ceph_data_storage:
+        data = {}
+        if ceph_service_flag:
             ceph_copy_num_default = nodes_info["cephCopyNumDefault"]
             service_type = nodes_info["serviceType"]
             nodes = nodes_info["nodes"]
             extend_pg_all = len(nodes) * len(self.ceph_data_storage) * 100
             all_pgs = self.get_deploy_history_pgs() + extend_pg_all
             data = self.calculate_ceph_storage(len(nodes), service_type, ceph_copy_num_default, all_pgs)
-
-        if self.local_storage:
-            data['localSizeMax'] = self.calculate_local_storage()
+        
+        if local_service_flag:
+            if self.local_storage:
+                data['localSizeMax'] = self.calculate_local_storage()
+            else:
+                data['localSizeMax'] = '0GB'
 
         return types.DataModel().model(code=0, data=data)
 
     def get_deploy_history_pgs(self):
-        try:
-            history_data = self.deploy_history_model.get_deploy_history()
-            datas_json = json.loads(history_data[1])
-            ceph_datas_num = 0
-            for node in datas_json['nodes']:
-                for storage in node['storages']:
-                    if storage['purpose'] == 'CEPH_DATA':
-                        ceph_datas_num += 1
-            return ceph_datas_num * 100
-        except Exception as e:
-            self._logger.error(
-                f"Get Deploy History file or Get storages in file is filed, Because: {e}")
-            raise
+        history_data = self.deploy_history_model.get_deploy_history()
+        datas_json = json.loads(history_data[1])
+        ceph_datas_num = 0
+        for node in datas_json['nodes']:
+            for storage in node['storages']:
+                if storage['purpose'] == 'CEPH_DATA':
+                    ceph_datas_num += 1
+        return ceph_datas_num * 100
 
 
 # 个性化pg计算
-class ExtendShowRecommendConfig(ExtendReckRecommendConfigCommon):
+class ExtendShowRecommendConfig(ExtendReckRecommendConfigCommon, ShowRecommendConfig):
     def post(self):
         nodes_info = self.get_nodes_from_request()
+        ceph_service_flag = nodes_info['cephServiceFlag']
+        local_service_flag = nodes_info['localServiceFlag']
         nodes = nodes_info["nodes"]
         for node in nodes:
             self.classify_disks(node['storages'])
 
-        if self.ceph_data_storage:
+        data = {}
+        if ceph_service_flag:
             ceph_copy_num_default = nodes_info["cephCopyNumDefault"]
             service_type = nodes_info["serviceType"]
             extend_pg_all = len(self.ceph_data_storage) * 100
@@ -60,7 +64,10 @@ class ExtendShowRecommendConfig(ExtendReckRecommendConfigCommon):
             data = self.calculate_ceph_storage(
                 1, service_type, ceph_copy_num_default, all_pgs)
         
-        if self.local_storage:
-            data['localSizeMax'] = self.calculate_local_storage()
+        if local_service_flag:
+            if self.local_storage:
+                data['localSizeMax'] = self.calculate_node_local_storage(nodes)
+            else:
+                data['localSizeMax'] = '0GB'
 
         return types.DataModel().model(code=0, data=data)
