b2f32541d9184f0bf46cd62e8b7da17962b12b11
caotingv <caotingv@qq.com>
2023-06-30 15:20:27 +0800

    update config.py.

--- app/config.py
+++ app/config.py
@@ -1,27 +1,30 @@
-PORT = 1236
-DEBUG = True
+from pathlib import Path
+import os
+
+# 获取当前工作目录的父目录
+DEPLOY_HOME = Path.cwd().parent
 
 # log 存放的目录
-LOG_PATH = '/var/log/deploy/'
-
-# 部署文件存放的位置
-DEPLOY_HOME = '/root/deploy/'
+LOG_PATH = os.path.join('/var/log/deploy', '')
 
 # 系统用户名和密码
 NODE_USER = 'root'
 NODE_PASS = 'Troila12#$'
 
 # 配置文件生成路径
-ETC_EXAMPLE_PATH = '/root/deploy/kly-deploy/etc_example/'
+ETC_EXAMPLE_PATH = DEPLOY_HOME.joinpath('kly-deploy/etc_example')
 
 # 模板存放路径
-TEMPLATE_PATH = '/root/deploy/kly-deploy-api/templates/'
+TEMPLATE_PATH = DEPLOY_HOME.joinpath('kly-deploy-api/templates')
 
 # shell 脚本存放路径
-SCRIPT_PATH = '/root/deploy/kly-deploy-api/scripts'
+SCRIPT_PATH = DEPLOY_HOME.joinpath('kly-deploy-api/scripts')
 
 # 升级文件存放位置
 UPGRADE_SAVE_PATH = '/opt/kly-upgrade/'
 
+PORT = 1236
+DEBUG = True
+
 # 数据库名称
-DB_NAME = '/root/deploy/kly-deploy.db'
+DB_NAME = DEPLOY_HOME.joinpath('kly-deploy.db')
